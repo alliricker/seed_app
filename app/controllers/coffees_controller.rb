a821630aa34def6a0910033e5beb541790d749d3
Allison Ricker <alliricker@gmail.com>
2022-07-06 12:54:07 -0600

    edit and update coffee

--- app/controllers/coffees_controller.rb
+++ app/controllers/coffees_controller.rb
@@ -1,7 +1,12 @@
 class CoffeesController < ApplicationController
     def new
+        if logged_in?
         @coffee = Coffee.new
         @coffee.categories.build
+        else 
+            flash[:error] = "Please Log In to Create a New Coffee"
+            redirect_to 'home'
+        end
     end
 
     def create
@@ -20,6 +25,18 @@ class CoffeesController < ApplicationController
     def show
         @coffee = Coffee.find_by_id(params[:id])
     end
+
+    def edit
+        @coffee= Coffee.find(params[:id])
+    end
+
+    def update 
+        @coffee = Coffee.find(params[:id])
+        @coffee.update(name: params[:coffee][:name], origin: params[:coffee][:origin], year: params[:coffee][:year])
+        redirect_to coffee_path(@coffee)
+    end
+
+
     
 private
 
